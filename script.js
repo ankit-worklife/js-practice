de69bf44c41f79b0799678e245b3cf3b6d49360a
Ankit Singh <94554151+ankit-worklife@users.noreply.github.com>
2025-11-28 09:14:13 +0000

    js practice

--- script.js
+++ script.js
@@ -7,11 +7,19 @@ accountCity = "Delhi"
 
 // accountId = 789456 we cannot redeclare const variable
 // But we can redeclare the let, var variables
+
 accountName = "Arpit"
 accountEmail = "ankit@yahoo.com" 
 accountPassword = 123456
 accountCity = "Mumbai"
+let accountState; // when we let any variable empty it is considered as undefined in js
+
+
+/* 
+Ignore var variable because 
+of scope issue and functional issue
+*/
 
 console.log(accountId);
-console.table([accountId, accountName, accountEmail, accountPassword, accountCity]);
+console.table([accountId, accountName, accountEmail, accountPassword, accountCity, accountState]);
 
